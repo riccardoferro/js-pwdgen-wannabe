435264abb359fe0745c56935f47fd353c2aa8150
riccardoferro <riccardo.ferron8@gmail.com>
2022-03-04 00:26:01 +0100

    completed exercise

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina e/o in console nomecognomecolorepreferito21
+
+const nome = prompt("inserisci il tuo nome:");
+const cognome = prompt("inserisci il tuo cognome");
+const ColorePreferito = prompt("inserisci il tuo colore preferito");
+console.log(
+  `
+  ${nome}${cognome}${ColorePreferito}${21}
+  `
+);
